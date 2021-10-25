d66d39d4fac60355b96908680a90abfe8f8dfb1a
islam <iabdelkarim@uplandsoftware.com>
2021-10-25 18:09:01 +0200

    update

--- src/routes/role.route.ts
+++ src/routes/role.route.ts
@@ -1,10 +1,38 @@
 import { Router } from "express";
 import { HttpError } from "../interfaces/http-error";
+import { IRole } from "../interfaces/role";
+import { addRole } from "../services/role.service";
 import { generateHttpResponse } from "../utils/utils";
 
 export const roleRouter = Router();
 
-roleRouter.post("/", async (req, res) => {});
+roleRouter.post("/", async (req, res) => {
+    try {
+        if(!req.body.name){
+            return res
+                .status(401)
+                .json(generateHttpResponse(401, "Missing 'name'"));
+        }
+        if(!req.body.permissions){
+            return res
+                .status(401)
+                .json(generateHttpResponse(401, "Missing 'permissions'"));
+        }
+        const role: IRole = {name: req.body.name, permissions: req.body.permissions};
+        const savedRole = await addRole(role);
+        return res.status(200)
+            .json(generateHttpResponse(200, "Authorized", savedRole));
+    } catch (error) {
+        if (error instanceof HttpError) {
+            return res
+              .status(error.code)
+              .json(generateHttpResponse(error.code, error.msg, error));
+        }
+        return res
+        .status(500)
+        .json(generateHttpResponse(500, "Unkown Error", error));
+    }
+});
 
 roleRouter.patch("/", async (req, res) => {});
 
--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,7 +1,19 @@
+import { HttpError } from "../interfaces/http-error";
 import { IRole } from "../interfaces/role";
+import { RoleModel } from "../models/role.model";
 
-export const addRole = (role: IRole) => {
+export const addRole = async (role: IRole) => {
   // check if role's permissions are valid
+  const permissions = await RoleModel.find({name: {$in: role.permissions}});
+  if (permissions.length === role.permissions.length) {
+    throw new HttpError(401, 'Please use valid permissions');
+  }
   // save
-  // throw an error if role already exist
+  try {
+    const doc = new RoleModel(role);
+    return await doc.save();
+  } catch (error) {
+    console.log(error);
+    throw new HttpError(500, "Failed to save to DB");
+  }
 };
